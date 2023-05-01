c09d1606da8f8f73542d6b451d4d82f85c3a5c18
Travis CI <travis@travis-ci.org>
2023-04-30 21:55:21 -0700

    Skeleton adding all hashing functions

---
+++ hasherProvider.go
@@ -0,0 +1,53 @@
+// MIT License
+//
+// Copyright (c) 2023 Godfrain Jacques Kounkou
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package hasherProvider
+
+import (
+    "fmt"
+
+	consistent "github.com/kounkou/hasherProvider/consistent"
+	random "github.com/kounkou/hasherProvider/random"
+	uniform "github.com/kounkou/hasherProvider/uniform"
+)
+
+type HasherProvider struct {
+	hasherMap map[int]interface{}
+}
+
+func (h *HasherProvider) GetHasher(hashFunction int) (interface{}, error) {
+	h.initHasherMap()
+	hasher, ok := h.hasherMap[hashFunction]
+
+	if !ok {
+		return nil, fmt.Errorf("unknown hashing function type: %d", hashFunction)
+	}
+	return hasher, nil
+}
+
+func (h *HasherProvider) initHasherMap() {
+	h.hasherMap = map[int]interface{}{
+		CONSISTENT_HASHING: consistent.ConsistentHashing{},
+		RANDOM_HASHING:     random.RandomHashing{},
+		UNIFORM_HASHING:    uniform.UniformHashing{},
+	}
+}
