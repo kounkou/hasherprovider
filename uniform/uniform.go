0b2381b8005c4aba2fb74d084d45d3473617baee
Godfrain Jacques <gkounkou@gmail.com>
2023-05-08 00:08:15 -0700

    Update uniform.go
    Fix typo

--- uniform/uniform.go
+++ uniform/uniform.go
@@ -50,7 +50,7 @@ func (h UniformHashing) Hash(uuid string, shards int) (string, error) {
 	return strconv.Itoa(hash % shards), nil
 }
 
-// Implemented for convenience, Uniformhashing does NOT support AddNode as the Randomhashing 
+// Implemented for convenience, Uniformhashing does NOT support AddNode as the Uniformhashing 
 // does NOT need to be ring like for Consistent Hashing.
 // This function will `panic`, as using this function in the client application is not an intended use of 
 // the Uniform Hashing algorithm
@@ -58,7 +58,7 @@ func (h UniformHashing) AddNode(_ string) {
     panic("AddNode method is not implemented for UniformHashing")
 }
 
-// Implemented for convenience, Uniformhashing does NOT support RemoveNode as the Randomhashing 
+// Implemented for convenience, Uniformhashing does NOT support RemoveNode as the Uniformhashing 
 // does NOT need to be ring like for Consistent Hashing.
 // This function will `panic`, as using this function in the client application is not an intended use of 
 // the Uniform Hashing algorithm
@@ -66,7 +66,7 @@ func (h UniformHashing) RemoveNode(_ string) {
     panic("RemoveNode method is not implemented for UniformHashing")
 }
 
-// Implemented for convenience, Uniformhashing does NOT support SetReplicas as the Randomhashing 
+// Implemented for convenience, Uniformhashing does NOT support SetReplicas as the Uniformhashing 
 // does NOT need to be ring like for Consistent Hashing.
 // This function will `panic`, as using this function in the client application is not an intended use of 
 // the Uniform Hashing algorithm
